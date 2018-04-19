40f485eb4d2b096a5957e48600826dcc261338ff
Vito Falco <vitofalco@gmail.com>
2018-04-19 09:31:53 +0200

    Fix #18

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,4 +136,19 @@ $(function () {
         });
     }
 
+    // SMOOTH INTERNAL LINKS
+    $("a[href!='#']").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+
+        window.location.hash = hash;
+      });
+    } 
+    });
+
 });
