ba54f5444ee5c2de26648d7cb62e64b85a2d6057
Francesco Zaia <francesco.zaia@gmail.com>
2018-05-21 09:54:59 +0200

    Pagina giornalisti (#49)
    * Implementazione pagina giornalisti WIP
    
    * Pagina giornalisti: incluso BaguetteBox, usate immagini jpg per slides e thumbs, rivista larghezza della pagina
    Caricamento scripts differenziato per homepage e pagine interne
    
    * Fix footer, font-size e link a forum

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,71 +12,76 @@ $(function () {
             el.remove();
         });
 
-        // Per ogni Path SVG esiste un'animazione senza autoplay
-        $('.carillon__path').each(function (index) {
+        if (window.anime) {
+            // Per ogni Path SVG esiste un'animazione senza autoplay
+            $('.carillon__path').each(function (index) {
 
-            var pathID = $(this).data('index');
-            var pathName = $(this).data('name');
-            var path = anime.path('[data-index="' + pathID + '"] path');
+                var pathID = $(this).data('index');
+                var pathName = $(this).data('name');
+                var path = anime.path('[data-index="' + pathID + '"] path');
 
-            var animeTimeline = anime.timeline({
-                targets: '[data-index="' + pathID + '"] .carillon__note',
-                easing: 'easeInOutQuart',
-                autoplay: false,
-                begin: function (anim) {
-                    $('.ball--' + pathName).addClass('ball--burst');
-                    $('[data-index="' + pathID + '"] .carillon__note').removeClass('carillon__note--burst');
-                },
-                complete: function () {
-                    $('.ball--' + pathName).removeClass('ball--burst');
-                    $('[data-index="' + pathID + '"] .carillon__note').addClass('carillon__note--burst').fadeTo("slow", 0);
-                    $('.carillon__sound-' + pathID)[0].play();
-                }
+                var animeTimeline = anime.timeline({
+                    targets: '[data-index="' + pathID + '"] .carillon__note',
+                    easing: 'easeInOutQuart',
+                    autoplay: false,
+                    begin: function (anim) {
+                        $('.ball--' + pathName).addClass('ball--burst');
+                        $('[data-index="' + pathID + '"] .carillon__note').removeClass('carillon__note--burst');
+                    },
+                    complete: function () {
+                        $('.ball--' + pathName).removeClass('ball--burst');
+                        $('[data-index="' + pathID + '"] .carillon__note').addClass('carillon__note--burst').fadeTo("slow", 0);
+                        $('.carillon__sound-' + pathID)[0].play();
+                    }
+                });
+
+                animeTimeline.add({
+                    opacity: 1,
+                    duration: 1000,
+                    delay: $(this).data('delay')
+                }).add({
+                    opacity: 1,
+                    translateX: path('x'),
+                    translateY: path('y'),
+                    duration: 1000
+                });
+
+                carillonPathArray.push(animeTimeline);
+
             });
-
-            animeTimeline.add({
-                opacity: 1,
-                duration: 1000,
-                delay: $(this).data('delay')
-            }).add({
-                opacity: 1,
-                translateX: path('x'),
-                translateY: path('y'),
-                duration: 1000
-            });
-
-            carillonPathArray.push(animeTimeline);
-        });
+        }
 
         // Sfondo in parallasse
-        if ($('.galaxy').length)
+        if (window.Parallax && $('.galaxy').length)
             parallaxInstance = new Parallax($('.galaxy')[0]);
 
         // ScrollMagic
-        var controller = new ScrollMagic.Controller();
-
-        // Sezioni 2, 3, 4, 5, 6
-        for (var _i = 2; _i < 7; _i++) {
-            (function (i) {
-                new ScrollMagic.Scene({
-                    triggerElement: ".section__" + i,
-                    duration: window.innerHeight - 50 // window.innerHeight - header height
-                })
-                    .on("enter", function () {
-                        if (i !== 1) {
-                            if (!smallDevice) {
-                                $(".section__" + i).addClass("active");
-                                $(".device__content--" + i).addClass("device__content--in");
-                            }
-                        }
+        if (window.ScrollMagic) {
+            var controller = new ScrollMagic.Controller();
+            
+            // Sezioni 2, 3, 4, 5, 6
+            for (var _i = 2; _i < 7; _i++) {
+                (function (i) {
+                    new ScrollMagic.Scene({
+                        triggerElement: ".section__" + i,
+                        duration: window.innerHeight - 50 // window.innerHeight - header height
                     })
-                    .on("leave", function () {
-                        if (!smallDevice) {
-                            $(".section__" + i).removeClass("active");
-                            $(".device__content--" + i).removeClass("device__content--in");
-                        }
-                    }).addTo(controller);
-            })(_i);
+                        .on("enter", function () {
+                            if (i !== 1) {
+                                if (!smallDevice) {
+                                    $(".section__" + i).addClass("active");
+                                    $(".device__content--" + i).addClass("device__content--in");
+                                }
+                            }
+                        })
+                        .on("leave", function () {
+                            if (!smallDevice) {
+                                $(".section__" + i).removeClass("active");
+                                $(".device__content--" + i).removeClass("device__content--in");
+                            }
+                        }).addTo(controller);
+                })(_i);
+            }
         }
 
         var greenDotAnimation = function () {
@@ -103,7 +108,8 @@ $(function () {
                 if (!smallDevice) {
                     if (!$('body').hasClass('is-scrolled')) {
                         // prende un Path SVG casuale nel carillon e ri-avvia l'animazione
-                        carillonPathArray[Math.floor(Math.random() * carillonPathArray.length)].restart();
+                        if (carillonPathArray.length)
+                            carillonPathArray[Math.floor(Math.random() * carillonPathArray.length)].restart();
                     } else {
                         // Imposta una posizione casuale la notifica "dot" verde
                         greenDotAnimation();
@@ -152,5 +158,8 @@ $(function () {
             });
         }
     });
+
+    if (window.baguetteBox)
+        window.baguetteBox.run('.giornalisti__slides-content');
 
 });
