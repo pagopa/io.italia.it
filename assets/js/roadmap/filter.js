bc97e8ce7fb260e7c58aa0fbddf62414e7cd7791
redbaron76 <f.fumis@gmail.com>
2019-11-21 15:32:03 +0100

    roadmap page and filter working

--- assets/js/roadmap/filter.js
+++ assets/js/roadmap/filter.js
@@ -1,6 +1,40 @@
 $(document).ready(function() {
   $(".filter-tool button").click(function() {
     var $btn = $(this);
-    console.log($btn.data("click"));
+    var $siblings = $btn.siblings();
+    var filter = $btn.data("click");
+
+    // switch on clicked button
+    $btn.removeClass("btn-outline-primary").addClass("btn-primary");
+    var $use = $btn.find("use");
+    if ($use && $use.length) {
+      var href = $use.attr("xlink:href");
+      $use.attr("xlink:href", href.replace("_b", "_w"));
+    }
+
+    // switch off sibiling buttons
+    $siblings.each(function() {
+      var $sibling = $(this);
+      $sibling.removeClass("btn-primary").addClass("btn-outline-primary");
+
+      var $sibUse = $sibling.find("use");
+      if ($sibUse && $sibUse.length) {
+        var sibHref = $sibUse.attr("xlink:href");
+        if (sibHref) {
+          $sibUse.attr("xlink:href", sibHref.replace("_w", "_b"));
+        }
+      }
+    });
+
+    // apply filter for each element
+    $(".point-list[data-filter]").each(function() {
+      var $point = $(this);
+      var id = $point.data("filter");
+      if (filter && filter !== id) {
+        $point.hide();
+      } else {
+        $point.show();
+      }
+    });
   });
 });
