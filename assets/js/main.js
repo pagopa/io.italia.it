18ab232e3b12011d7474c90780efb0ac1afe4cf6
Vito Falco <vitofalco@gmail.com>
2020-03-05 09:59:58 +0100

    Getione video homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,4 +165,40 @@ $(function () {
     $('.js-nl-groups .js-nl-group-option')
         .on('change', onFieldsChange);
 
+    // HOMEPLAYER
+    $("#playvideo").click( function(e) {
+        e.preventDefault();
+        var src = $(this).attr("data-url");
+        $(".overlay-video").show();
+        setTimeout(function() {
+            $(".overlay-video").addClass("o1");
+            $("#player").attr("src", src);
+        }, 100);
+    });
+    
+    $(".overlay-video").click(function(event) {
+        if (!$(event.target).closest(".videoWrapperExt").length) {
+            var PlayingVideoSrc = $("#player").attr("src").replace("&autoplay=1", "");
+            $("#player").attr("src", PlayingVideoSrc);
+            $(".overlay-video").removeClass("o1");
+            setTimeout(function() {
+                $(".overlay-video").hide();
+            }, 600);
+        }
+    });
+    
+    $(".videoWrapper__close").click(function(event) {
+            var PlayingVideoSrc = $("#player").attr("src").replace("&autoplay=1", "");
+            $("#player").attr("src", PlayingVideoSrc);
+            $(".overlay-video").removeClass("o1");
+            setTimeout(function() {
+                $(".overlay-video").hide();
+            }, 600);
+    
+    });
+
 });
+
+
+
+
