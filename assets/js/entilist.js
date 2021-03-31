2c1afe320808289ea8bc0dc73a4644cf99a578cf
Vito Falco <vitofalco@gmail.com>
2021-03-31 16:51:26 +0200

    ricerca in enti

--- assets/js/entilist.js
+++ assets/js/entilist.js
@@ -1,6 +1,54 @@
 $(function() {
     var enti = ".entiList__item";
+    var $enti = $(enti);
     var $filters = $(".entiList__scopefilter a");
+    var $entiSearchInput = $("#entiSearchInput");
+    var jsonEntiUrl = "/assets/json/servizi-ricercabili.json";
+    var searchableList = [];
+
+    $.ajax({
+        url: jsonEntiUrl,
+        dataType: 'json',
+        success: function(data,textStatus,xhr) {
+            data.forEach( function (element, index, array) {
+                var orgName = element["o"].toUpperCase();
+                var fc = element["fc"].toString();
+                var toIndex = orgName + "|" + fc;
+                searchableList.push(toIndex);
+            });
+        }
+    });
+
+    function delay(callback, ms) {
+        var timer = 0;
+        return function() {
+          var context = this, args = arguments;
+          clearTimeout(timer);
+          timer = setTimeout(function () {
+            callback.apply(context, args);
+          }, ms || 0);
+        };
+      }
+
+    function search(stringSearched) {
+        var resultsFC = [];
+        results = searchableList.filter(function(entry) {
+            if ( entry.indexOf(stringSearched.toUpperCase()) !== -1) {
+                resultsFC.push(entry.split("|")[1]);
+                return true;
+            } else {
+                return false;
+            }
+        });
+        $enti.each( function(index) {
+            var $ente = $(this);
+            if (resultsFC.includes( $ente.attr('id'))) {
+                $ente.fadeIn();
+            } else {
+                $ente.fadeOut();
+            }
+        })
+    }
 
     $filters.on("click", function(e) {
         e.preventDefault();
@@ -13,5 +61,19 @@ $(function() {
         }
         $filters.removeClass("active");
         $(this).addClass("active");
-    })
+    });
+
+    $entiSearchInput.on("focus", function() {
+        $filters.first().trigger("click");
+    });
+
+    $entiSearchInput.on("keyup", delay(function(e) {
+        e.preventDefault();
+        var value = this.value;
+        if (value.length < 3) {
+            $enti.fadeIn();
+            return false;
+        }
+        search(value);
+    },1000));
 });
