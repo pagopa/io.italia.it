3192a7042570eaa80d9742e8eb2b3f965cc6609d
Mauro Melis <mau.2m@hotmail.it>
2022-12-20 13:05:30 +0100

    feat(home): get enti and services kpis from dashboard data

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,11 @@
-var INFOURL = "/assets/json/enti-list-numbers.json";
+var INFOURL = "https://pdnd-prod-dl-1-public-data.s3.eu-central-1.amazonaws.com/dashboard/pagopa/dashboard-io.json";
 
 $(window).on("load", function() {
     $enti = $("#home-enti-num");
     $services = $("#home-servizi-num");
     $.get( INFOURL, function( data ) {
-        $enti.text(data.enti);
-        $services.text(data.services);
+        $enti.text(data.enti_total && data.enti_total.toLocaleString("it"));
+        $services.text(data.services_total && data.services_total.toLocaleString("it"));
       }, "json")
       .fail(function() {
         $enti.parent().parent().remove();
