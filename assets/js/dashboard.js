1c125e9101e083943d07b9d1e569a7f327da097c
mauromelis <mauromelis@users.noreply.github.com>
2023-01-12 12:12:38 +0100

    [PDL-3012] Add dashboard archive json (#452)
    * feat(scripts): add dashboard data archive
    
    * feat(dashboard): update to v2.0.0

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -5,7 +5,10 @@ $(function () {
     }, 0);
 
     $("#downloads").text(totalDownloads.toLocaleString("it"));
-    $("#bonusTotal").text(dashboardData.generatedBonusCount.toLocaleString("it"));
-    $("#bonusAmount").text(dashboardData.generatedBonusAmount.toLocaleString("it") + " €");
+  });
+
+  $.getJSON("https://pdnd-prod-dl-1-public-data.s3.eu-central-1.amazonaws.com/dashboard/pagopa/dashboard-io-archive.json", function (dashboardArchiveData) {
+    $("#bonusTotal").text(dashboardArchiveData.generatedBonusCount.toLocaleString("it"));
+    $("#bonusAmount").text(dashboardArchiveData.generatedBonusAmount.toLocaleString("it") + " €");
   });
 });
