2703573c1435219ee4c89c796fe39a2a5b06ed4c
Vito Falco <vitofalco@gmail.com>
2023-06-12 11:43:13 +0200

    Drop analytics. Add Matomo

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,32 +1,24 @@
-var targCookiesGroup = "C0002";
-var checkValue = targCookiesGroup + '%3A1';
-var OTCookieValue = document.cookie;
+// MATOMO
+var _paq = (window._paq = window._paq || []);
 
-
-function ga_init() {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-162375189-1', {
-        'cookieExpires': 60 * 60 * 24 * 28 * 6 // meno di 6 mesi
-    });
-    ga('set', 'anonymizeIp', true);
-    ga('send', 'pageview');
-}
-
-function OptanonWrapper() {
-    /* wrapping in OneTrust callback function */
-    OneTrust.OnConsentChanged(function () {
-        var activeGroups = OnetrustActiveGroups;
-        if (activeGroups.indexOf(targCookiesGroup) > -1) {
-            ga_init();
-        }
-      });
-
-}
-
-if (OTCookieValue.indexOf(checkValue) > -1) {
-    ga_init();
-}
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+(function () {
+  var u = "https://pagopa.matomo.cloud/";
+  _paq.push(["setTrackerUrl", u + "matomo.php"]);
+  _paq.push(["setSiteId", "4"]);
+  var d = document,
+    g = d.createElement("script"),
+    s = d.getElementsByTagName("script")[0];
+  g.async = true;
+  g.src = "/assets/js/matomo.js";
+  s.parentNode.insertBefore(g, s);
+})();
+// this function removes the "old" google analytics cookies
+document.addEventListener("DOMContentLoaded", function () {
+  var cookies = Cookies.get();
+  var gaCookies = Object.keys(cookies).filter((k) => k.startsWith("_ga"));
+  for (var gc in gaCookies) {
+    Cookies.remove(gaCookies[gc], { path: "" });
+  }
+});
