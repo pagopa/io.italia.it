6f9896e3959ad567fa32a6c44876daecebbc442a
Vito Falco <vitofalco@gmail.com>
2024-01-26 10:38:11 +0100

    Use "oj" as library to manage json's dump (#498)

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'jekyll-redirect-from', '~> 0.16.0'
 gem 'kramdown-parser-gfm', '~> 1.1.0'
 gem "webrick", "~> 1.7"
 gem 'down', '~> 5.2'
+gem 'oj', '~> 3.16.3'
 gem "jekyll-minifier", "~> 0.1.10"
 gem 'jekyll-datapage-generator', '~> 1.3.0'
 gem 'rss', '~> 0.2.9'
--- _plugins/generateEntiList.rb
+++ _plugins/generateEntiList.rb
@@ -4,6 +4,7 @@ THIS SCRIPT IS USEFUL TO GENERATE A NEW JSON WITH ENTI'S DATA
 =end
 require 'json'
 require 'down'
+require 'oj'
 
 # If the pipeline isn't scheduled, then break the flow
 if ENV['PIPELINE_TYPE']=='BatchedCI' && ENV['JEKYLL_ENV']=='production'
@@ -120,10 +121,12 @@ def renderEntiList(file, site)
 
     enti_searchable_sorted = enti_searchable.sort
 
-    File.write('./assets/json/enti-list-webview.json', JSON.dump( new_content_webview.values.sort_by{ |hsh| hsh.values[0] } ))
-    File.write('./assets/json/enti-list-searchable.json', JSON.dump(enti_searchable_sorted))
-    File.write('./assets/json/enti-list-numbers.json', JSON.dump(numbers_info))
-    File.write('_data/enti-servizi.json', JSON.dump(new_content))
+    Oj.default_options = {:mode => :compat , :ascii_only => true}
+
+    File.write('./assets/json/enti-list-webview.json', Oj.dump( new_content_webview.values.sort_by{ |hsh| hsh.values[0] } ))
+    File.write('./assets/json/enti-list-searchable.json', Oj.dump(enti_searchable_sorted))
+    File.write('./assets/json/enti-list-numbers.json', Oj.dump(numbers_info))
+    File.write('_data/enti-servizi.json', Oj.dump(new_content))
 end
 
 Jekyll::Hooks.register :site, :after_init do |site|
